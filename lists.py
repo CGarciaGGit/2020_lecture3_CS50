6c36591b90730453f0c7374496c5605f6e6ca4fd
CGarciaGGit <carlos_a_garcia_garza@hotmail.com>
2021-10-21 23:30:44 -0500

    initial release lists, append and sort function

---
+++ lists.py
@@ -0,0 +1,12 @@
+#define a list of names
+name = ["Harry", "Ron", "Hermione", "Ginny" ]
+
+print(name)
+print(name[1])
+print(name[2])
+
+name.append("Draco")
+print(name[4])
+
+name.sort()
+print(name)
