393875e285a5d3ac5bf442340a06db4e8b2f0fcb
CGarciaGGit <carlos_a_garcia_garza@hotmail.com>
2021-10-21 23:17:24 -0500

    modify with new ways to show  harrays

--- sequences.py
+++ sequences.py
@@ -3,6 +3,11 @@ name = ["Harry", "Ron", "Hermione" ]
 print(name[0])
 print(name[1])
 print(name[2])
-print(name[3])
-print(name[4])
 
+name2 = "harrys"
+
+print(name2[0])
+print(name2[1])
+print(name2[2])
+print(name2[3])
+print(name2[4])
