203e23d4946af6928de99370c71a3bcf9763ccc4
CGarciaGGit <carlos_a_garcia_garza@hotmail.com>
2021-10-21 23:22:21 -0500

    its a change example of how to perform arrays

--- sequences.py
+++ sequences.py
@@ -1,13 +1,2 @@
-name = ["Harry", "Ron", "Hermione" ]
-
-print(name[0])
-print(name[1])
-print(name[2])
-
-name2 = "harrys"
-
-print(name2[0])
-print(name2[1])
-print(name2[2])
-print(name2[3])
-print(name2[4])
+coordinateX = 10.0
+coordinateX = 20.0
